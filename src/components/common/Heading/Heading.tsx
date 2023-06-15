2c2dfc8605877d3567ae15837a153226f8fa95c2
Muhammad Usman <musman0741@gmail.com>
2023-06-15 18:01:56 +0500

    [feature/homepage-basic-layout] new "Heading" component added

---
+++ src/components/common/Heading/Heading.tsx
@@ -0,0 +1,29 @@
+// React Dependencies
+import React from "react";
+
+// External Dependencies
+import { tv } from "tailwind-variants";
+
+type Props = {
+  text: string;
+  variant: string;
+};
+
+const heading = tv({
+  base: "text-4.5xl leading-9",
+  variants: {
+    type: {
+      mainHeading: "",
+      heading: "",
+      subHeading: "",
+    },
+  },
+});
+
+export const Heading = ({ text, variant }: Props) => {
+  return <h1 className={heading({ type: variant })}>{text}</h1>;
+};
+
+Heading.defaultProps = {
+  variant: "heading",
+};
